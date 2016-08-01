ebc32fcb0ec0ac14da27ea998daad14fd17cbb8d
Chad Brandon <cbrandon@yodle.com>
2016-08-01 14:31:07 -0600

    Initial bootable app

---
+++ src/main/java/edu/duke/bioinformatics/Application.java
@@ -0,0 +1,16 @@
+package edu.duke.bioinformatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class Application
+{
+    public static void main(String[] args) throws Exception
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
